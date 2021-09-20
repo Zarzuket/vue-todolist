6c97d6089457ca1124bb88933a6a464c906506e2
Zarzuket <davide.bajo@gmail.com>
2021-09-20 16:00:57 +0200

    add css + todo list

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+      todos: [
+        "Comprare cibo per Paprika",
+        "Finire gli esercizi",
+        "Fare la spesa",
+        "Nerdare"
+      ]
+    },
+    methods: {
+
+    }
+  })
