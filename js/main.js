9ea4fd3c537d59761286e06f42df5f12ccbfde91
Zarzuket <davide.bajo@gmail.com>
2021-09-20 18:00:17 +0200

    add checkmark

--- js/main.js
+++ js/main.js
@@ -2,22 +2,41 @@ const app = new Vue({
     el: '#root',
     data: {
       todos: [
-        "Comprare cibo per Paprika",
-        "Finire gli esercizi",
-        "Fare la spesa",
-        "Nerdare"
+        {
+          titolo :"Comprare cibo per Paprika",
+          stato : false
+        },
+        {
+          titolo :"Finire gli esercizi",
+          stato : false
+        },
+        {
+          titolo :"Fare la spesa",
+          stato : false
+        },
+        {
+          titolo :"Nerdare",
+          stato : false
+        }
       ],
-      newTodo: ""
+      stringTodo : ""
     },
     methods: {
       addTodo: function(){
-        if (this.newTodo != ""){
-          this.todos.push(this.newTodo);
-          this.newTodo = "";
+        if (this.stringTodo != ""){
+          let newTodo = {
+            titolo : this.stringTodo,
+            stato : false,
+          }
+          this.todos.push(newTodo);
         }
+
       },
       removeTodo: function(index){
         this.todos.splice(index,1);
+      },
+      todoDone: function(index){
+        this.todos[index].stato = true;
       }
     }
   })
